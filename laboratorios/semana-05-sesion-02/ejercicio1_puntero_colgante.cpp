1195d19a622b360373e03d766e2c204c6f605457
juan <juanandreschalcualan@gmail.com>
2026-09-16 07:58:59 -0500

    clase 2 semana 5

--- laboratorios/semana-05-sesion-02/ejercicio1_puntero_colgante.cpp
+++ laboratorios/semana-05-sesion-02/ejercicio1_puntero_colgante.cpp
@@ -2,6 +2,10 @@
 
 // TODO: predice antes de compilar. ¿que advertencia esperas que de
 // el compilador sobre esta funcion?
+
+// RESPUESTA: El compilador advertirá que se está devolviendo la dirección 
+// de una variable local. Al salir de la función, esa memoria se libera 
+// por lo que el puntero quedará apuntando a la nada osea el puntero colgante
 int* obtenerLecturaInsegura(int valorSensor) {
     int lectura = valorSensor * 2;
     return &lectura;
